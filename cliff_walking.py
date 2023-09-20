a599cf53db4e8e1e46078460928d0c101673c0f6
raymond <raymondshawn779@gmail.com>
2023-09-20 10:38:34 -0600

    implemented train method

--- cliff_walking.py
+++ cliff_walking.py
@@ -1,8 +1,9 @@
 import gymnasium as gym
 import numpy as np
-env=gym.make("CliffWalking-v0")
-actions=env.action_space.n
-states=env.observation_space.n
+from tqdm  import tqdm
+env = gym.make("CliffWalking-v0")
+actions = env.action_space.n
+states = env.observation_space.n
 
 # Training parameters
 n_training_episodes = 10000  # Total training episodes
@@ -12,7 +13,7 @@ learning_rate = 0.7  # Learning rate
 n_eval_episodes = 100  # Total number of test episodes
 
 # Environment parameters
-env_id = "FrozenLake-v1"  # Name of the environment
+
 max_steps = 99  # Max steps per episode
 gamma = 0.95  # Discounting rate
 eval_seed = []  # The evaluation seed of the environment
@@ -23,13 +24,14 @@ min_epsilon = 0.05  # Minimum exploration probability
 decay_rate = 0.0005  # Exponential decay rate for exploration prob
 
 
-def initialise_Qtable(states,actions):
-    q_table=np.zeros((states,actions))
+def initialise_Qtable(states, actions):
+    q_table = np.zeros((states, actions))
     return q_table
 
 
-Q_table=initialise_Qtable(4,12)
+Q_table = initialise_Qtable(4, 12)
 print(Q_table)
+
 
 def greedy_policy(Qtable, state):
     # Exploitation: take the action with the highest state, action value
@@ -43,7 +45,7 @@ def epsilon_greedy_policy(Qtable, state, epsilon):
     random_num = np.random.uniform(0, 1)
     # if random_num > greater than epsilon --> exploitation
     if random_num > epsilon:
-        
+
         action = greedy_policy(Qtable, state)
     # else --> exploration
     else:
@@ -52,3 +54,34 @@ def epsilon_greedy_policy(Qtable, state, epsilon):
     return action
 
 
+def train(n_training_episodes, min_epsilon, max_epsilon, decay_rate, env, max_steps, Qtable):
+    for episode in tqdm(range(n_training_episodes)):
+        # Reduce epsilon (because we need less and less exploration)
+        epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay_rate * episode)
+        # Reset the environment
+        state, info = env.reset()
+        step = 0
+        terminated = False
+        truncated = False
+
+        # repeat
+        for step in range(max_steps):
+            # Choose the action At using epsilon greedy policy
+            action = epsilon_greedy_policy(Qtable, state, epsilon)
+
+            # Take action At and observe Rt+1 and St+1
+            # Take the action (a) and observe the outcome state(s') and reward (r)
+            new_state, reward, terminated, truncated, info = env.step(action)
+
+            # Update Q(s,a):= Q(s,a) + lr [R(s,a) + gamma * max Q(s',a') - Q(s,a)]
+            Qtable[state][action] = Qtable[state][action] + learning_rate * (
+                reward + gamma * np.max(Qtable[new_state]) - Qtable[state][action]
+            )
+
+            # If terminated or truncated finish the episode
+            if terminated or truncated:
+                break
+
+            # Our next state is the new state
+            state = new_state
+    return Qtable

a8d93adad139a5b4d0f22776c2a557f25f6af1c4
raymond <raymondshawn779@gmail.com>
2023-09-20 10:31:26 -0600

    implemented greedy and epsilon greedy policies

--- cliff_walking.py
+++ cliff_walking.py
@@ -2,13 +2,53 @@ import gymnasium as gym
 import numpy as np
 env=gym.make("CliffWalking-v0")
 actions=env.action_space.n
-states=env.observation_space
+states=env.observation_space.n
 
+# Training parameters
+n_training_episodes = 10000  # Total training episodes
+learning_rate = 0.7  # Learning rate
 
+# Evaluation parameters
+n_eval_episodes = 100  # Total number of test episodes
+
+# Environment parameters
+env_id = "FrozenLake-v1"  # Name of the environment
+max_steps = 99  # Max steps per episode
+gamma = 0.95  # Discounting rate
+eval_seed = []  # The evaluation seed of the environment
+
+# Exploration parameters
+max_epsilon = 1.0  # Exploration probability at start
+min_epsilon = 0.05  # Minimum exploration probability
+decay_rate = 0.0005  # Exponential decay rate for exploration prob
 
 
 def initialise_Qtable(states,actions):
-    q_table=np.zeros(states,actions)
+    q_table=np.zeros((states,actions))
     return q_table
+
+
+Q_table=initialise_Qtable(4,12)
+print(Q_table)
+
+def greedy_policy(Qtable, state):
+    # Exploitation: take the action with the highest state, action value
+    action = np.argmax(Qtable[state][:])
+
+    return action
+
+
+def epsilon_greedy_policy(Qtable, state, epsilon):
+    # Randomly generate a number between 0 and 1
+    random_num = np.random.uniform(0, 1)
+    # if random_num > greater than epsilon --> exploitation
+    if random_num > epsilon:
+        
+        action = greedy_policy(Qtable, state)
+    # else --> exploration
+    else:
+        action = env.action_space.sample()
+
+    return action
 
 

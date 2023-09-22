167876ee34676529db0a6161b009953ab478cd4b
raymond <raymondshawn779@gmail.com>
2023-09-22 15:38:45 -0600

    play indefinitely

--- GridWorld/main.py
+++ GridWorld/main.py
@@ -1,22 +1,30 @@
 import tkinter as tk
-from tkinter import font
+from tkinter import font,messagebox
 import random
-class GridWindow:
+import time
+class GridWindow():
     def __init__(self, rows, columns):
+        super().__init__()
         self.rows = rows
         self.columns = columns
         self.cells = []  # List to store the buttons and values
+        self.window=tk.Tk()
+        
 
 
-    def button_click(self,event):
+    def run(self):
+        self.window.mainloop()
+    def button_click(self,row,col):
         #print(self.get_cell_value(row,column))
-        print("click")
+        print(row,col)
+       
+        print(f"event click")
     def create_grid(self):
         # Create the main window
-        window = tk.Tk()
+       
 
         # Define a larger font size
-        custom_font = font.Font(size=14)
+        # custom_font = font.Font(size=14)
 
         # Create a grid of buttons with specified number of rows and columns
         for i in range(self.rows):
@@ -24,14 +32,14 @@ class GridWindow:
             for j in range(self.columns):
                 value = tk.StringVar()  # Variable to hold the value of the cell
                 value.set(0)  # Set initial value to zero
-                button = tk.Button(window, textvariable=value, padx=50, pady=50, relief=tk.RIDGE, font=custom_font)
+                button = tk.Button(self.window, textvariable=value, padx=50, pady=50, relief=tk.RIDGE,command=lambda r=i, c=j: self.button_click(r, c))
                 button.grid(row=i, column=j)
-                button.bind("<Button-1>",self.button_click)
+                
                 row.append((button, value))
             self.cells.append(row)
-
+        return self.cells
         # Run the main event loop
-        window.mainloop()
+       
 
     def get_cell_value(self, row, column):
         return self.cells[row][column][1].get()
@@ -39,23 +47,15 @@ class GridWindow:
     def set_cell_value(self, row, column, value):
         self.cells[row][column][1].set(value)
         
-    def play(self):
-        # Generate random coordinates
-        random_row = random.randint(0, 4)
-        random_column = random.randint(0, 4)
 
-# Get the randomly selected button
-        random_cell = self.cells[random_row][random_column]
-
-# Programmatically trigger the click event on the randomly selected button
-        random_cell.invoke()
 
 # Create an instance of GridWindow with 4 rows and 4 columns
 grid_window = GridWindow(4, 4)
+grid=grid_window.create_grid()
 
-# Call the create_grid method to create the grid
-grid_window.create_grid()
-grid_window.play()
+
+
+
 """
 # Access and modify a cell in the grid
 cell_2_3_value = grid_window.get_cell_value(1, 2)
@@ -67,10 +67,39 @@ print(updated_value)  # Prints the updated value of the cell at row 2, column 3
 """
 
 class Game:
-    def __init__(self,grid:GridWindow):
-        self.grid=grid
-        
+    def __init__(self,gridwindow:GridWindow):
+        super().__init__()
+        self.grid_window=gridwindow
+        self.window=self.grid_window.window
+        self.grid=gridwindow.create_grid()
+        self.goal=self.grid[3][3]
+        self.clicked=None
+        self.finished=False
+    def run(self):
+        self.window.mainloop()        
     def play(self):
-        pass
+        
+        while True:
+            self.window.update()
+            row=random.randint(0,3)
+            col=random.randint(0,3)
+            if row==3 and col==3:
+                print(f"end {row,col}")
+                break
+            random_cell=self.grid[row][col][0]
+            #self.window.after(0,random_cell.invoke)
+            random_cell.invoke()
+            random_cell.configure(background="blue")
+            
+            print(f'in play {row,col}')
+            time.sleep(3)  
+            
     def end(self):
         pass
+    
+    
+game=Game(grid_window)
+#game.play()
+
+game.play()
+game.run()
